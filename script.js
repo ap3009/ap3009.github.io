dd914f4d0b118fae5b991d27186d706df950f160
ap3009 <77648239+ap3009@users.noreply.github.com>
2021-11-06 23:08:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
-//const targetNumber = 0;
-// Write your code below:
-//Task 3
-const generateTarget = () =>
-{
+const generateTarget = () => {
   targetNumber = Math.floor(Math.random()*10);
   return targetNumber;
 }
-
-//Task 4
 const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
-  /*
-  let compDiff = Math.abs(targetNumber - computerGuess);
-
-  let humanDiff = Math.abs(targetNumber - humanGuess);*/
-  //Task 8
+  if(humanGuess >=10)
+    alert("Please Guess between 0 - 9!");
+  
   let {compDiff,humanDiff} = getAbsoluteDistance(humanGuess, computerGuess, targetNumber);
-  //console.log(compDiff);
-
-  if (humanDiff < compDiff){
+  
+  if (humanDiff < compDiff)
     return true;
-  }
-  else if (compDiff  > humanDiff){
+  else if (compDiff  > humanDiff)
     return false;
-  }
-  else if(compDiff === humanDiff){
-    return true; //tied and human should win.
-  }
+  else if(compDiff === humanDiff)
+    return true; //tied and human should win.  
 }
 
-//Task 5
 const updateScore = (winner) => {
   if(winner == 'human')
     humanScore +=1;
@@ -39,12 +26,7 @@ const updateScore = (winner) => {
     computerScore +=1;
 }
 
-//Task 6
-const advanceRound = () => {
-currentRoundNumber +=1;
-}
-
-//Task 8
+const advanceRound = () => currentRoundNumber +=1;
 
 const getAbsoluteDistance = (humanGuess,computerGuess, targetNumber) => {
   compDiff = Math.abs(targetNumber - computerGuess);
